3e3b3b76689f5b33191fc41b63b9eafcd0bcd1a4
Ilya Boyur <ilya.boyur.r@ya.ru>
2022-07-08 14:56:39 +0300

    Add database model

--- backend/boenergo_app/models.py
+++ backend/boenergo_app/models.py
@@ -3,3 +3,28 @@ items = None
 BLUE = 'rgb(18, 121, 255)'
 GREEN = 'rgb(28, 129, 28)'
 RED = 'rgb(255, 18, 69)'
+
+from django.core import validators
+from django.db import models
+
+class Item(models.Model):
+    number = models.PositiveSmallIntegerField(
+        'Порядковый номер'
+    )
+    is_revealed = models.BooleanField(
+        'Раскрыт ли реальный цвет'
+    )
+    color_guess = models.PositiveIntegerField(
+        'Предполагаемый цвет в HEX',
+        validators=(
+            validators.MaxValueValidator(0xFFFFFF,
+                                         'Превышен максимум для цвета'),
+        ),
+    )
+    color_actual = models.PositiveIntegerField(
+        'Реальный цвет в HEX',
+        validators=(
+            validators.MaxValueValidator(0xFFFFFF,
+                                         'Превышен максимум для цвета'),
+        ),
+    )
